b7e06b353be8a8e461c5b626465ec48154779817
Yashika1615 <katariay22@gmail.com>
2024-05-09 20:39:58 +0530

    url-shortner-app

---
+++ mern stack project/url-shortner/server-app/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const port= process.env.PORT || 5001;
+
+const app= express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
